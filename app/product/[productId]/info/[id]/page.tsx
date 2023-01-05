f452aff7485575c8fdfef098487749eab1a7dc1d
Jack Hanford <jackhanford@gmail.com>
2023-01-05 09:13:59 -0800

    add async request to product/id/info/id

--- app/product/[productId]/info/[id]/page.tsx
+++ app/product/[productId]/info/[id]/page.tsx
@@ -1,9 +1,18 @@
-export default function RootPage({ params: { productId, id } }) {
+export default async function RootPage({ params: { productId, id } }) {
+  const data = await getData();
+
   return (
     <div>
       <h1>
         Product Info page: {productId} - {id}
       </h1>
+      <div>{data.body}</div>
     </div>
   );
+}
+
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const data = await res.json();
+  return data;
 }
